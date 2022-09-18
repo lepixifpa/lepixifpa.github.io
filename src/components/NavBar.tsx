72dfa8db233db2164e24320eafab8d98e6e12c05
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-09-18 16:58:09 -0300

    correcao nav bar e divisao redes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 export const NavBar = () => {
     return (
         <nav>
-            <img
-                src="https://raw.githubusercontent.com/lepixifpa/lepixifpa/main/logo-round.png"
-                alt=""
-                width="40px"
-            />
-            <a href="body">Home</a>
+            <a href="body">
+                <img
+                    src="https://raw.githubusercontent.com/lepixifpa/lepixifpa/main/logo-round.png"
+                    alt=""
+                    width="40px"
+                    />
+                    Home
+            </a>
             <a href="#participantes">Participantes</a>
             <a href="#redes">Redes sociais</a>
         </nav>
