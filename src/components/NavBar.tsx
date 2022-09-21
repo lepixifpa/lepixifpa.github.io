37ba9a85e6602e9a6328988cf235e9bf05ed39a4
Vitor Koch <vitorgabrielkoch@gmail.com>
2022-09-21 17:41:34 -0300

    mudanca de fonte e formatacao do codigo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,16 @@
 export const NavBar = () => {
-    return (
-        <nav>
-            <a href="body">
-                <img
-                    src="https://raw.githubusercontent.com/lepixifpa/lepixifpa/main/logo-round.png"
-                    alt=""
-                    width="40px"
-                    />
-                    Home
-            </a>
-            <a href="#participantes">Participantes</a>
-            <a href="#redes">Redes sociais</a>
-        </nav>
-    );
-};  
+  return (
+    <nav> 
+      <a href="body">
+        <img
+          src="https://raw.githubusercontent.com/lepixifpa/lepixifpa/main/logo-round.png"
+          alt=""
+          width="40px"
+        />
+        Home
+      </a>
+      <a href="#participantes">Participantes</a>
+      <a href="#redes">Redes sociais</a>
+    </nav>
+  );
+};
