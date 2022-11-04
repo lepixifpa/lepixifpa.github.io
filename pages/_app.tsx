fe9fdeb284029e6c25ba10660efb5a1da647bb12
Vitor <vitorgabrielkoch@gmail.com>
2022-11-04 20:31:09 -0300

    corrigido titulo mudando em compartilhar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import GlobalStyle from "../styles/globals";
 import styled from "styled-components";
 import type { AppProps } from "next/app";
 import { NavBar } from "../components/NavBar";
-import NextNProgress from "nextjs-progressbar"
-import "../styles/fonts.css"
+import NextNProgress from "nextjs-progressbar";
+import "../styles/fonts.css";
+import Head from "next/head";
 
 const StyledFooter = styled.footer`
   color: white;
@@ -14,6 +15,15 @@ const StyledFooter = styled.footer`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>LEPIX</title>
+        <meta
+          name="description"
+          content="Nós somos o LEPIX (Laboratório Experimental de Pesquisa em Informática do Xingu), um grupo de estudantes do IFPA Campus Altamira estudando Arduino, Machine Learning e Data Mining"
+        />
+        <link rel="short icon" href="/logo-round.png" />
+      </Head>
+
       <GlobalStyle />
       <NavBar />
       <NextNProgress />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,15 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import { Descricao } from '../components/Descricao'
 import { Redes } from '../components/Redes'
 import { Participantes } from '../components/Participantes'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Head>
-        <title>LEPIX</title>
-        <meta
-          name="description"
-          content="Nós somos o LEPIX (Laboratório Experimental de Pesquisa em Informática do Xingu), um grupo de estudantes do IFPA Campus Altamira estudando Arduino, Machine Learning e Data Mining"
-        />
-        <link
-          rel="short icon"
-          href="/logo-round.png"
-        />
-      </Head>
-
+    <>
       <Descricao />
       <Redes />
       <Participantes />
-    </div>
+    </>
   );
 }
 export default Home;
